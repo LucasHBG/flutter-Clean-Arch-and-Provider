c4fe4877a9c3fbfe581243a8d92829fb3fa60e9f
Lucas Guimarães <lukahbg@gmail.com>
2022-07-20 00:21:52 -0300

    [FEAT]: implemented go_router functions to main and a errorPage for unknown routes

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,29 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:go_router/go_router.dart';
 import 'package:test_app/login_screen.dart';
 
 Future<void> main() async {
+  final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/login',
+        pageBuilder: (context, state) => MaterialPage(
+          key: state.pageKey,
+          child: const LoginScreen(),
+        ),
+      ),
+    ],
+    //Handle all errors related to a route that dont exists
+    errorPageBuilder: (context, state) => MaterialPage(
+      key: state.pageKey,
+      child: Scaffold(
+        body: Center(child: Text(state.error.toString())),
+      ),
+    ),
+  );
+
   WidgetsFlutterBinding.ensureInitialized();
 
   runZonedGuarded<Future<void>>(
@@ -15,10 +35,11 @@ Future<void> main() async {
       );
 
       runApp(
-        const MaterialApp(
+        MaterialApp.router(
+          routeInformationParser: router.routeInformationParser,
+          routerDelegate: router.routerDelegate,
           debugShowCheckedModeBanner: false,
           themeMode: ThemeMode.system,
-          home: LoginScreen(),
         ),
       );
     },
