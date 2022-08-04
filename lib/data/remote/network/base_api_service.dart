e4e3d4f1dd88b676507aef85e24a6c00764cfcad
Lucas Guimarães <lukahbg@gmail.com>
2022-08-04 00:43:01 -0300

    [feat]: add fake json api and BaseApiService class

---
+++ lib/data/remote/network/base_api_service.dart
@@ -0,0 +1,7 @@
+abstract class BaseApiService {
+  ///! [INFO]: Temporary _fake api_ URL
+  final String baseUrl = "https://jsonplaceholder.typicode.com";
+
+  ///Method to be implemented by extended classes
+  Future<dynamic> getResponse(String url);
+}
