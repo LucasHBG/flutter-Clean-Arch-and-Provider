82bfbd714e0138e74f96ae1ec7d359ba48478e9b
Lucas Guimarães <lukahbg@gmail.com>
2022-08-05 12:13:46 -0300

    [chore]: refactoring utils class

--- lib/utils/constants.dart
+++
@@ -1,8 +0,0 @@
-// Success
-const int success = 200;
-
-// Errors
-const int userInvalidResponse = 100;
-const int noInternet = 101;
-const int invalidFormat = 102;
-const int unknownError = 103;
---
+++ lib/utils/utils.dart
@@ -0,0 +1,4 @@
+/// Class responsible to handle all reusable utilities.
+/// Every method should be `static` in order to be accessed
+/// by outside classes.
+class Utils {}
