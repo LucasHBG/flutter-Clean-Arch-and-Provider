3e157900fe4fed58bbf824a03a5e4bc8385430c5
Lucas Guimarães <lukahbg@gmail.com>
2022-08-10 01:25:34 -0300

    [feat]: add UserList mgmt page with Provider - Initialized the widget with fetchUsers() function; - add ChangeNotifierProvider to listen to changes; - add Consumer<UserListVM> to listen to changes from model; - add ListView.builder to lazily load our list of users

---
+++ lib/view/user_list_mgmt_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:test_app/models/users_list/users_list_model.dart';
+
+import '../data/remote/response/status.dart';
+import '../view_models/user_view_model.dart';
+import 'widgets/my_error_widget.dart';
+import 'widgets/my_loading_widget.dart';
+
+class UserListMgmtPage extends StatefulWidget {
+  const UserListMgmtPage({Key? key}) : super(key: key);
+
+  @override
+  State<UserListMgmtPage> createState() => _UserListMgmtPageState();
+}
+
+class _UserListMgmtPageState extends State<UserListMgmtPage> {
+  final UsersListVM usersListVM = UsersListVM();
+
+  @override
+  void initState() {
+    usersListVM.fetchUsers();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: const Text('User Maneagment Page')),
+        body: ChangeNotifierProvider(
+          create: (BuildContext context) => usersListVM,
+          child: Consumer<UsersListVM>(
+            builder: (context, value, _) {
+              switch (usersListVM.usersList.status) {
+                case Status.loading:
+                  debugPrint("Entered status :: LOADING");
+                  return const MyLoadingWidget();
+                case Status.error:
+                  debugPrint("Entered status :: ERROR");
+                  return MyErrorWidget(usersListVM.usersList.message ?? "NA");
+                case Status.completed:
+                  debugPrint("Entered status :: COMPLETED");
+                  return _getUsersListView(usersListVM.usersList.data?.users);
+
+                default:
+              }
+
+              return Container();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget _getUsersListView(List<UserModel>? usersList) {
+  return ListView.builder(
+    itemCount: usersList?.length,
+    itemBuilder: (context, int index) {
+      return _getUserListItem(usersList![index]);
+    },
+  );
+}
+
+Widget _getUserListItem(UserModel user) {
+  return Card(
+    child: ListTile(
+      title: Text(user.name),
+      subtitle: Text(user.email),
+    ),
+  );
+}
