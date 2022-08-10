d55420729cd47fc21f78437c30b4dfe2a090d979
Lucas Guimarães <lukahbg@gmail.com>
2022-08-10 01:26:50 -0300

    [fix]: UserListVM using old name for UserListModel

--- lib/view_models/user_view_model.dart
+++ lib/view_models/user_view_model.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:test_app/data/remote/response/api_response.dart';
-import 'package:test_app/models/users_list/users_main_model.dart';
+import 'package:test_app/models/users_list/users_list_model.dart';
 import 'package:test_app/repositories/user_repository_imp.dart';
 
 class UsersListVM extends ChangeNotifier {
   final _userRepository = UserRepositoryImp();
 
-  ApiResponse<UsersMainModel> usersMain = ApiResponse.loading();
+  ApiResponse<UsersListModel> usersList = ApiResponse.loading();
 
-  void _setUsersListObject(ApiResponse<UsersMainModel> response) {
-    print("Entered setUsersObj :: $response");
-    usersMain = response;
+  void _setUsersListObject(ApiResponse<UsersListModel> response) {
+    debugPrint("Entered setUsersObj :: $response");
+    usersList = response;
     notifyListeners();
   }
 
