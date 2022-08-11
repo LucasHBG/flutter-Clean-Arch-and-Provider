875bfc04ad4e9986bbdeb15127430f931a5908bf
Lucas Guimarães <lukahbg@gmail.com>
2022-08-11 16:52:38 -0300

    [chore]: refactoring method name to _setUsersList

--- lib/view_models/users_list_vm.dart
+++ lib/view_models/users_list_vm.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:test_app/data/remote/response/api_response.dart';
 import 'package:test_app/models/users_list/users_list_model.dart';
 import 'package:test_app/repositories/user_repository_imp.dart';
@@ -8,19 +8,19 @@ class UsersListVM extends ChangeNotifier {
 
   ApiResponse<UsersListModel> usersList = ApiResponse.loading();
 
-  void _setUsersListObject(ApiResponse<UsersListModel> response) {
+  void _setUsersList(ApiResponse<UsersListModel> response) {
     debugPrint("Entered setUsersObj :: $response");
     usersList = response;
     notifyListeners();
   }
 
   Future<void> fetchUsers() async {
-    _setUsersListObject(ApiResponse.loading());
+    _setUsersList(ApiResponse.loading());
     _userRepository
         .getUsersList()
         .then(
-            (value) => _setUsersListObject(ApiResponse.completed(data: value)))
+            (value) => _setUsersList(ApiResponse.completed(data: value)))
         .onError((error, stackTrace) =>
-            _setUsersListObject(ApiResponse.error(message: error.toString())));
+            _setUsersList(ApiResponse.error(message: error.toString())));
   }
 }
