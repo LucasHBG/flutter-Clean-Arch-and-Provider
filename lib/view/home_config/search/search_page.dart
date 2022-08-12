ea46714c4e48766d36d9ce248e112f5786e6857a
Lucas Guimarães <lukahbg@gmail.com>
2022-08-12 01:33:30 -0300

    [feat]: add Search page

---
+++ lib/view/home_config/search/search_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Search Page'),
+    );
+  }
+}
