92937a2d6fb22113720cb2ccd18c91e6784e59c3
Lucas Guimarães <lukahbg@gmail.com>
2022-08-12 01:33:15 -0300

    [feat]: add YourLibrary page

---
+++ lib/view/home_config/your_library/your_library_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class YourLibraryPage extends StatelessWidget {
+  const YourLibraryPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Your Library Page'));
+  }
+}
