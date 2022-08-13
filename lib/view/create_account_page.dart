d04c668eab7fffcfb60ad6e0f9c7909d2412e21f
Lucas Guimarães <lukahbg@gmail.com>
2022-08-13 16:32:19 -0300

    [feat]: add basic CreateAccountPage

---
+++ lib/view/create_account_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CreateAccountPage extends StatelessWidget {
+  const CreateAccountPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Create acc page'));
+  }
+}
