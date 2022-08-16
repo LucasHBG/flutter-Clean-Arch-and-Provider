ce9f480c0b2a4190ee480a8db310adb9aeb8a0a5
Lucas Guimarães <lukahbg@gmail.com>
2022-08-16 11:06:05 -0300

    [fix]: remove unsed imports

--- lib/view/home_config/collection/collection_page.dart
+++ lib/view/home_config/collection/collection_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class CollectionPage extends StatelessWidget {
   const CollectionPage({Key? key}) : super(key: key);
--- lib/view/home_config/search/search_page.dart
+++ lib/view/home_config/search/search_page.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({Key? key}) : super(key: key);
